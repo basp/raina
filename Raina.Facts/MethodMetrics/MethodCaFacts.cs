60a62fe9089f2b8517f573b24d76594c0abeb956
Bas Pennings <baspster@gmail.com>
2018-02-03 13:19:28 +0100

    MethodCa tests

--- Raina.Facts/MethodMetrics/MethodCaFacts.cs
+++ Raina.Facts/MethodMetrics/MethodCaFacts.cs
@@ -5,6 +5,35 @@ namespace Raina.Facts.MethodMetrics
 
     public class MethodCaFacts
     {
-        
+        [Fact]
+        public void CalculateMethodCa()
+        {
+            var type = typeof(MethodCaFacts);
+            var cases = new []
+            {
+                Tuple.Create(2, type.GetMethodDefinition(nameof(this.Method1)).MethodCa()),
+                Tuple.Create(2, type.GetMethodDefinition(nameof(this.Method2)).MethodCa()),
+                Tuple.Create(0, type.GetMethodDefinition(nameof(this.Method3)).MethodCa()),
+            };
+        }
+
+        private void Method1()
+        {
+            this.Method2();
+            Console.WriteLine("foo");
+        }
+
+        private void Method2()
+        {
+            this.Method1();
+            this.Method2();
+        }
+
+        private void Method3()
+        {
+            this.Method1();
+            this.Method2();
+            this.Method3();
+        }
     }
 }
