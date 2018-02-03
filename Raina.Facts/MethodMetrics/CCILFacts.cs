9191bded4f4cca2bcb292c166fb203b48eb6bbe6
Bas Pennings <baspster@gmail.com>
2018-02-03 13:51:18 +0100

    Tests for CCIL

--- Raina.Facts/MethodMetrics/CCILFacts.cs
+++ Raina.Facts/MethodMetrics/CCILFacts.cs
@@ -12,8 +12,9 @@ namespace Raina.Facts.MethodMetrics
             var cases = new[]
             {
                 Tuple.Create(1, type.GetMethodDefinition(nameof(this.Method1))),
-                Tuple.Create(1, type.GetMethodDefinition(nameof(this.Method2))),
-                Tuple.Create(1, type.GetMethodDefinition(nameof(this.Method3))),
+                Tuple.Create(2, type.GetMethodDefinition(nameof(this.Method2))),
+                Tuple.Create(3, type.GetMethodDefinition(nameof(this.Method3))),
+                Tuple.Create(5, type.GetMethodDefinition(nameof(this.Method4))),
             };
 
             Array.ForEach(cases, x => Assert.Equal(x.Item1, x.Item2.CCIL()));
@@ -25,10 +26,50 @@ namespace Raina.Facts.MethodMetrics
 
         private void Method2()
         {
+            var x = 0;
+
+            if (x > 100)
+            {
+                Console.WriteLine("foo");
+            }
         }
 
         private void Method3()
         {
+            var x = 0;
+
+            if (x > 100)
+            {
+                Console.WriteLine("foo");
+            }
+            else
+            {
+                Console.WriteLine("bar");
+            }
+        }
+
+        private void Method4()
+        {
+            var x = 0;
+            var y = 0;
+
+            if (x > 100)
+            {
+                Console.WriteLine("foo");
+            }
+            else
+            {
+                Console.WriteLine("bar");
+
+                if (y < 100)
+                {
+                    Console.WriteLine("quux");
+                }
+                else
+                {
+                    Console.WriteLine("frotz");
+                }
+            }
         }
     }
 }
