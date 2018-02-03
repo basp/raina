bcde4d99329250e97db5c3836aab11cbd8520da3
Bas Pennings <baspster@gmail.com>
2018-02-03 13:39:07 +0100

    Fixing tests

--- Raina.Facts/MethodMetrics/MethodCaFacts.cs
+++ Raina.Facts/MethodMetrics/MethodCaFacts.cs
@@ -12,10 +12,11 @@ namespace Raina.Facts.MethodMetrics
             var cases = new[]
             {
                 Tuple.Create(2, type.GetMethodDefinition(nameof(this.Method1))),
-                Tuple.Create(2, type.GetMethodDefinition(nameof(this.Method2))),
-                Tuple.Create(0, type.GetMethodDefinition(nameof(this.Method3))),
+                Tuple.Create(3, type.GetMethodDefinition(nameof(this.Method2))),
+                Tuple.Create(1, type.GetMethodDefinition(nameof(this.Method3))),
             };
 
+            Array.ForEach(cases, x => Assert.Equal(x.Item1, x.Item2.MethodCa()));
         }
 
         private void Method1()
