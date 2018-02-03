1fd4237b9e393f198b81fb3f6e86ea6d9ce7d256
Bas Pennings <baspster@gmail.com>
2018-02-03 13:43:13 +0100

    More tests

--- Raina.Facts/MethodMetrics/CCILFacts.cs
+++ Raina.Facts/MethodMetrics/CCILFacts.cs
@@ -5,5 +5,30 @@ namespace Raina.Facts.MethodMetrics
 
     public class CCILFacts
     {
+        [Fact]
+        public void CalculateCCIL()
+        {
+            var type = typeof(CCILFacts);
+            var cases = new[]
+            {
+                Tuple.Create(1, type.GetMethodDefinition(nameof(this.Method1))),
+                Tuple.Create(1, type.GetMethodDefinition(nameof(this.Method2))),
+                Tuple.Create(1, type.GetMethodDefinition(nameof(this.Method3))),
+            };
+
+            Array.ForEach(cases, x => Assert.Equal(x.Item1, x.Item2.CCIL()));
+        }
+
+        private void Method1()
+        {
+        }
+
+        private void Method2()
+        {
+        }
+
+        private void Method3()
+        {
+        }
     }
 }
