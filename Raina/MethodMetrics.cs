7d4d31e2858242f498a0f890ecae1f234935a1fe
Bas Pennings <baspster@gmail.com>
2018-02-05 17:30:54 +0100

    Some hacking in the sourcecode analyzer (which as expected turns out to be not so solid)

--- Raina/MethodMetrics.cs
+++ Raina/MethodMetrics.cs
@@ -30,11 +30,7 @@ namespace Raina
 
         // The number of scopes in a method.
         public static int ILNestingDepth(this MethodDefinition self) =>
-            // We can reasonbly guestimate this by just counting the number
-            // of conditional branch instructions in the body.
-            self.Body.Instructions
-                .Where(x => x.IsConditionalBranchInstruction())
-                .Count();
+            self.HasBody ? self.CountBranchInstructions() : 0;
 
         // The number of methods that depend on this method.
         // External methods are not counted.
@@ -49,22 +45,10 @@ namespace Raina
         // The number of methods that this method depends on.
         // External methods are counted.
         public static int MethodCe(this MethodDefinition self) =>
-            self.Body.Instructions
-                .Where(x => x.IsMethodCall())
-                .Where(x => x.HasWellKnownCallOperand())
-                .Select(x => x.GetOperandMethodFullName())
-                // Doing a distinct on the method's full name isn't really
-                // very elegant but it works well.
-                .Distinct()
-                .Count();
+            self.HasBody ? self.CountMethodCalls() : 0;
 
         public static int CCIL(this MethodDefinition self) =>
-            self.Body.Instructions
-                .Where(x => x.IsBranchInstruction())
-                .Select(x => (Instruction)x.Operand)
-                .Select(x => x.Offset)
-                .Distinct()
-                .Count() + 1;
+            self.HasBody ? self.CountJumpOffsets() : 1;
 
         // The only thing that makes this a little bit tricky is that we
         // don't want to count the curly braces marking the beginning and
@@ -80,19 +64,48 @@ namespace Raina
         {
             var memread = MemoizeReadDocument();
             return self.DebugInformation.SequencePoints
-                .Select(x => new { Code = memread(x.Document.Url), Ins = x })
+                .Select(x => new
+                {
+                    Url = x.Document.Url,
+                    Code = memread(x.Document.Url),
+                    Ins = x
+                })
                 // We sometimes get crazy (outlier) line numbers from Cecil (like 16234212 in a
                 // 96 line file) so let's just hack around this for now and pretend it did not
                 // happen...
                 .Where(x => x.Code.IsValidLine(x.Ins.StartLine))
                 // This is actually a pretty dirty way to skip around the scope markers but
                 // it's straightforward and works well enough for C# based assemblies.
-                .Select(x => x.Code.GetCharAt(x.Ins.StartLine, x.Ins.StartColumn))
+                .Select(x => x.Code.GetCharAt(x.Ins.StartLine, x.Ins.StartColumn, x.Url))
                 .Where(x => !x.IsScopeMarker())
                 // This will give a pretty good indication of the actual lines of code without
                 // any real biases.
                 .Count();
         }
+
+        private static int CountBranchInstructions(this MethodDefinition self) =>
+            self.Body.Instructions
+                .Where(x => x.IsConditionalBranchInstruction())
+                .Count();
+
+        private static int CountMethodCalls(this MethodDefinition self) =>
+            self.Body.Instructions
+                .Where(x => x.IsMethodCall())
+                .Where(x => x.HasWellKnownCallOperand())
+                .Select(x => x.GetOperandMethodFullName())
+                // Doing a distinct on the method's full name isn't really
+                // very elegant but it works well.
+                .Distinct()
+                .Count();
+
+
+        private static int CountJumpOffsets(this MethodDefinition self) =>
+            self.Body.Instructions
+                .Where(x => x.IsBranchInstruction())
+                .Select(x => (Instruction)x.Operand)
+                .Select(x => x.Offset)
+                .Distinct()
+                .Count() + 1;
 
         private static string GetOperandMethodFullName(this Instruction self)
         {
@@ -150,8 +163,26 @@ namespace Raina
             line >= 0 && line < self.Count();
 
         // Line and col are one-based, substract one to indices
-        private static char GetCharAt(this IEnumerable<string> self, int line, int col) =>
-            self.ElementAt(line - 1).Substring(col - 1, 1)[0];
+        private static char GetCharAt(this IEnumerable<string> self, int line, int col, string url)
+        {
+            // FIXME
+            // This method is a mess...
+            // It seems when you have a property like: public int Count { get; set; }
+            // on one line, the thing gets confused and it will arrive on the next line,
+            // not able to find the '}' and just error out.
+            var t = self.ElementAt(line - 1);
+            var startIndex = col - 1;
+            if (startIndex >= 0 && startIndex < t.Length)
+            {
+                return t.Substring(startIndex, 1)[0];
+            }
+
+            // Below is just debugging crud, this shouldn't really happen
+            var prev = line > 1 ? self.ElementAt(line - 2) : t;
+            var curr = t;
+            var msg = $"Can't find position line {line} and column {col} in {url} ({prev.Trim()}) ({curr.Trim()})";
+            throw new InvalidOperationException(msg);
+        }
 
         private static IEnumerable<string> ReadDocument(this string self) =>
             File.ReadLines(self);
